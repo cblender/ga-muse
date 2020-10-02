0111dcc6fcf42ce66d739c29db6d9a7114a68638
cblender <christopher.blender@gmail.com>
2020-10-02 12:15:50 -0400

    untracked node_modules.  Added react routes.

--- muse/src/App.js
+++ muse/src/App.js
@@ -1,27 +1,35 @@
 import React from "react";
-import axios from "axios";
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header.js";
+import Gallery from "./components/Gallery.js";
+import List from "./components/List.js";
+import axios from "axios";
+import { Route, HashRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>TEST INSERT. PLEASE DELETE ME.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter basename="/">
+      <div className="app">
+        <Header />
+        <main>
+          <Route
+            exact
+            path="/"
+            render={({ match }) => <Gallery match={match} />}
+          />
+          <Route
+            exact
+            path="/gallery"
+            render={({ match }) => <Gallery match={match} />}
+          />
+          <Route
+            exact
+            path="/list"
+            render={({ match }) => <List match={match} />}
+          />
+        </main>
+      </div>
+    </HashRouter>
   );
 }
 
