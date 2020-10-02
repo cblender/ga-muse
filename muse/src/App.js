66c7aa02da93ddf8e0b765376b9d65ee967c9596
cblender <christopher.blender@gmail.com>
2020-10-02 10:51:22 -0400

    installed react and axios.  ready for frontend development

--- muse/src/App.js
+++ muse/src/App.js
@@ -11,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>TEST INSERT. PLEASE DELETE ME.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
