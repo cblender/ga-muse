027a34a0eeb4a690fbc513b9cac69ebba339deda
iandemed <iandemed@gmail.com>
2020-10-02 12:44:20 -0400

    Extend the URL field to accept more characters

--- muse/models.py
+++ muse/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Gallery(models.Model):
 
     # Set the length of a gallery title to the length of a tweet
@@ -8,10 +9,13 @@ class Gallery(models.Model):
     def __str__(self):
         return self.title
 
+
 class Image(models.Model):
     name = models.CharField(max_length=140)
-    img_url = models.URLField(max_length=280) # this will allow for longer image urls to be entered
-    gallery_id = models.ForeignKey(Gallery, on_delete=models.CASCADE, related_name="images")
+    # this will allow for longer image urls to be entered
+    img_url = models.URLField(max_length=480)
+    gallery_id = models.ForeignKey(
+        Gallery, on_delete=models.CASCADE, related_name="images")
 
     def __str__(self):
         return self.name
